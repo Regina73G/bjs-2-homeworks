ee05249a383310413380f6f04d366982e2781d30
Regina Salikhova <yanadera1873@gmail.com>
2025-02-17 16:00:29 +0400

    added the solution of tasks for lecture 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let d = b ** 2 - 4 * a * c;
+	if (d === 0) {
+		let x = -b / (2 * a);
+		arr.push(x);
+	} else if (d > 0) {
+		let x1 = (-b + Math.sqrt(d)) / (2 * a);
+		let x2 = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(x1);
+		arr.push(x2);
+	}
+	console.log(arr)
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let rate = (percent / 100) / 12;
+	let body = amount - contribution;
+	let monthlyPayment = body * (rate + (rate / (((1 + rate) ** countMonths) - 1)));
+	let sum = Number((monthlyPayment * countMonths).toFixed(2));
+	console.log(sum);
+	return sum;
 }
