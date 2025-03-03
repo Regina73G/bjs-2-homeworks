5f9086b9a4ae13caca140bfe65c902a908a06c69
Regina Salikhova <yanadera1873@gmail.com>
2025-03-03 18:19:53 +0400

    Minor fix

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -11,18 +11,16 @@ Student.prototype.setSubject = function (subjectName) {
 
 Student.prototype.addMarks = function (...marks) {
   if (this.excluded) {
-    return 0;
-  }else {
-    this.marks = [...this.marks, ...marks];
+    return;
   }
+  this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function () {
   if (!this.hasOwnProperty("marks") || this.marks.length === 0) {
     return 0;
-  } else {
-    return this.marks.reduce((accumulator, currentValue) => accumulator + currentValue, 0) / this.marks.length;
   }
+  return this.marks.reduce((accumulator, currentValue) => accumulator + currentValue, 0) / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
